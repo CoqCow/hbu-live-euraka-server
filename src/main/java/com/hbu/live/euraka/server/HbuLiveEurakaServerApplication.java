e94a9644fa091c114ef3e8db42b324fc52fea67b
e <1004337524@qq.com>
2018-06-17 11:12:21 +0800

    first commit

---
+++ src/main/java/com/hbu/live/euraka/server/HbuLiveEurakaServerApplication.java
@@ -0,0 +1,12 @@
+package com.hbu.live.euraka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbuLiveEurakaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbuLiveEurakaServerApplication.class, args);
+	}
+}
